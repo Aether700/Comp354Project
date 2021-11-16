c38cf11577af3701db45fef1a7440ab7172645b5
Aether700 <69548363+Aether700@users.noreply.github.com>
2021-11-15 20:05:48 -0500

    Created the Parser singleton class with the checkUserInput function

---
+++ src/NAR/Parser.java
@@ -0,0 +1,43 @@
+package NAR;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser 
+{
+	private static Parser Instance = new Parser();
+	
+	private List<HighLevelFunc> constructs;
+	
+	private Parser() 
+	{
+		constructs = new ArrayList<HighLevelFunc>();
+		
+		//initialize and add HighLevelFunc objects here 
+	}
+	
+	public static boolean checkUserInput(String statement) 
+	{
+		List<HighLevelFunc> constructs = Instance.constructs;
+		
+		for (HighLevelFunc func : constructs) 
+		{
+			if (func.isCalled(statement)) 
+			{
+				if (func.isCorrectSyntax(statement)) 
+				{
+					func.setArgs(statement);
+					func.execute();
+					return true;
+				}
+				//if the statement is being called and we don't have a 
+				//correct syntax stop looking for constructs and return false
+				return false;
+			}
+		}
+		
+		//if no valid construct found print an error message and return false
+		String calledConstructName = statement.split(" ")[0];
+		Editor.printToConsole("Unknown construct '" + calledConstructName + "' found.");
+		return false;
+	}
+}
