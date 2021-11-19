6c7479fd906f0ea890ffa9957ce6cc3863594de1
Aryamann <aryamanmehra98@gmail.com>
2021-11-19 18:18:31 -0500

    removed some (hopefully) useless files and fixed square root syntax

--- src/NAR/Constructs/SquareRootConstruct.java
+++ src/NAR/Constructs/SquareRootConstruct.java
@@ -8,12 +8,12 @@ public class SquareRootConstruct extends HighLevelFunc {
     private int op1, op2, result;
 
     public SquareRootConstruct() {
-        super("sqrt");
+        super("square");//putting square root gives unknown construct
     }
     @Override
     public boolean isCorrectSyntax(String statement) {
         String lowerCase = statement.toLowerCase();
-        if (lowerCase.matches("^sqrt \\d+$")) {
+        if (lowerCase.matches("^square root of \\d+$")) {
             return true;
             //check syntax of the construct to execute when the condition is true
         }
@@ -39,7 +39,7 @@ public class SquareRootConstruct extends HighLevelFunc {
 
 
     public static void main(String args[]) {
-        String statement = "sqrt 0";
+        String statement = "square root of 9";
         Editor.run();
         Parser.checkUserInput(statement);
     }
@@ -54,9 +54,9 @@ public class SquareRootConstruct extends HighLevelFunc {
         String[] components = new String[2];
 
 
-        int first = str.indexOf("sqrt ");
+        int first = str.indexOf("square root of ");
 
-        components[0] = str.substring(first + 5).trim(); //retrieve condition
+        components[0] = str.substring(first + 15).trim(); //retrieve condition
 
 
         return components;
