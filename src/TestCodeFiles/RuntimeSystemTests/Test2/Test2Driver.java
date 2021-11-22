ca64db75cd7afd8fd9a8df5500d6cb91cb2fab8a
Shae Bparsa <65192419+ShaeBparsa@users.noreply.github.com>
2021-11-22 18:40:24 -0500

    Update Test2Driver.java

--- src/TestCodeFiles/RuntimeSystemTests/Test2/Test2Driver.java
+++ src/TestCodeFiles/RuntimeSystemTests/Test2/Test2Driver.java
@@ -1,16 +1,27 @@
 package TestCodeFiles.RuntimeSystem.Test2;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test2Driver {
 	public static void main(String[] args) {
 		
-		String st = "add 78 to 9 \n" + "         \n" + "                      \n"
-				+ " add  99   to  5                \n" + " add               7 to 4                         \n"
-				+ "                       wrong\n" + "\n" + "\n"
-				+ "add   7 to                                        78j\n" + "         \n";
-
-		RuntimeSystem.runCode(st);
-
+		List<String> inputs=new ArrayList<String>();  
+		
+		inputs.add("statement\n"+"statement\n"+""+"wrong\n"+"statement\n"); //input 1
+		inputs.add("wrong\n"+"statement\n"+"statement\n"+"statement\n"+"statement\n"); //input 2
+		inputs.add("statement\n"+"statement\n"+"wrong\n"+"wrong\n"+"statement\n"+"statement\n"); //input 3
+		inputs.add(""); //input 4
+		inputs.add("                                  \n"+"                 \n"); //input 5
+		
+		int i=1;
+		
+		for (String input:inputs) {
+			
+		System.out.println("output of input "+i++ +":\n");
+		RuntimeSystem.runCode(input);
+		System.out.println();
+		
+		}
 	}
 }
