15819327a34cb115fa65eb997e15228ef0cc67ca
Shae Bparsa <65192419+ShaeBparsa@users.noreply.github.com>
2021-11-21 22:20:35 -0500

    Update Test1Driver.java

--- src/TestCodeFiles/RuntimeSystemTests/Test1/Test1Driver.java
+++ src/TestCodeFiles/RuntimeSystemTests/Test1/Test1Driver.java
@@ -1,20 +1,16 @@
-import NAR.Editor;
+package TestCodeFiles.RuntimeSystem.Test1;
 
+//In runTime test1 we assume all the input commands have correct syntax
 
-public class Test1Driver {
-	public static void main(String[] args) 
-	{
-		String st ="add 78 to 9\n"
-				+ " add  99   to  5      \n"
-				+ "                      \n"
-				+ "           \n"
-				+ " add 7 to 4                         \n" + "                               \n"+ "\n"
-				+ "\n"
-				+ "addd hh j\n"
-				+ "         \n";
+public class Test2Driver {
+	public static void main(String[] args) {
 		
-		RuntimeSysObj.runCode(st); //comment and uncomment this line to test with or without the HighLevelFunction objects in the parser
-		//Editor.run();
+		String st = "add 78 to 9 \n" + "                      \n" + "                      \n"
+				+ " add  99   to  5                \n" + " add               7 to 4                         \n"
+				+ "                               \n" + "\n" + "\n"
+				+ "add   7 to                                        78j\n" + "         \n";
+
+		RuntimeSystem.runCode(st);
 
 	}
 }
