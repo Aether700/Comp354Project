5279df5f15e7729aa71b1ae9dd8d39db67bc8d37
Shae Bparsa <65192419+ShaeBparsa@users.noreply.github.com>
2021-11-22 19:13:47 -0500

    Update Test1Driver.java

--- src/TestCodeFiles/RuntimeSystemTests/Test1/Test1Driver.java
+++ src/TestCodeFiles/RuntimeSystemTests/Test1/Test1Driver.java
@@ -1,16 +1,27 @@
-package TestCodeFiles.RuntimeSystem.Test1;
+package TestCodeFiles.RuntimeSystem.Test2;
 
+import java.util.ArrayList;
+import java.util.List;
 //In runTime test1 we assume all the input commands have correct syntax
-
 public class Test2Driver {
 	public static void main(String[] args) {
 		
-		String st = "add 78 to 9 \n" + "                      \n" + "                      \n"
-				+ " add  99   to  5                \n" + " add               7 to 4                         \n"
-				+ "                               \n" + "\n" + "\n"
-				+ "add   7 to                                        78j\n" + "         \n";
-
-		RuntimeSystem.runCode(st);
-
+		List<String> inputs=new ArrayList<String>();  
+		
+		inputs.add("statement1\n"+"statement2\n"+"statement3\n"+"                      \n"+"                      \n"); //input 1
+		inputs.add("                                statement1\n"+" statement2        \n"+"      \n"+"     \n"+"      statement3 \n"); //input 2
+		inputs.add("       statement1\n"+"       statement2\n"+"         "+"\n"+"      statement3\n     "); //input 3
+		inputs.add("                          "); //input 4
+		inputs.add(""); //input 5
+		
+		int i=1;
+		
+		for (String input:inputs) {
+			
+		System.out.println("output of input "+i++ +":\n");
+		RuntimeSystem.runCode(input);
+		System.out.println();
+		
+		}
 	}
 }
