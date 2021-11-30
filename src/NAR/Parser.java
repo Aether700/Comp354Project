f0af17149a245b08fb82ec86d60a556ee3d4b371
Aryamann <aryamanmehra98@gmail.com>
2021-11-30 12:58:41 -0500

    Updated with main's parser to make it work w variables

--- src/NAR/Parser.java
+++ src/NAR/Parser.java
@@ -1,27 +1,38 @@
 package NAR;
-import NAR.Constructs.*;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import NAR.Constructs.AddConstruct;
+import NAR.Constructs.DivideConstruct;
+import NAR.Constructs.IfElseConstruct;
 
 public class Parser 
 {
 	private static Parser Instance = new Parser();
 	
 	private List<HighLevelFunc> constructs;
+	private HashMap<String, Double> variables;
 	
 	private Parser() 
 	{
 		constructs = new ArrayList<HighLevelFunc>();
+		variables = new HashMap<String, Double>();
 		
-		//initialize and add HighLevelFunc objects here
-		constructs.add(new AddConstruct());
-		constructs.add(new SubtractConstruct());
-		constructs.add(new MultiplyConstruct());
+		//initialize and add HighLevelFunc objects here 
+		constructs.add(new IfElseConstruct());
 		constructs.add(new DivideConstruct());
-		constructs.add(new SquareRootConstruct());
+		constructs.add(new AddConstruct());
 	}
 	
+	/**
+	 * This method gets some input string as its parameters. 
+	 * This method checks for valid construct and syntax to execute the relevant HighLevelFunc object. 
+	 * If no valid HighLevelFunc object is found or if the syntax for said object is incorrect, 
+	 * the method returns false, otherwise true.  
+	 * @param statement
+	 * @return
+	 */
 	public static boolean checkUserInput(String statement) 
 	{
 		List<HighLevelFunc> constructs = Instance.constructs;
@@ -46,5 +57,45 @@ public class Parser
 		String calledConstructName = statement.split(" ")[0];
 		Editor.printToConsole("Unknown construct '" + calledConstructName + "' found.");
 		return false;
+	}
+	
+	/**
+	 * This method takes a variable name as input.
+	 * Returns true if the variable has been defined false otherwise.
+	 * @param variable
+	 * @return
+	 */
+	public static boolean isVarDefined(String variable) {
+		return Instance.variables.containsKey(variable);
+	}
+	
+	/**
+	 * This method takes two parameters, a variable name, and it's associated number value. 
+	 * It stores the variable name and number together in internal memory. 
+	 * It also can be used to store a new value for an already defined variable.
+	 * @param variable
+	 * @param value
+	 */
+	public static void defineVar(String variable, double value) {
+		Instance.variables.put(variable, value);
+	}
+	
+	/**
+	 * This method takes a variable name as input and 
+	 * returns the numeric value associated with it.
+	 * @param variable
+	 * @return value of variable
+	 */
+	public static double getVarValue(String variable) {
+		return Instance.variables.get(variable);
+	}
+	
+	/**
+	 * Returns the list of HighLevelFunc objects supported by the parser/application. 
+	 * This function is used by the editor when displaying syntax information for the user in the output console.
+	 * @return
+	 */
+	public static List<HighLevelFunc> getFuncList(){
+		return Instance.constructs;
 	}
 }
